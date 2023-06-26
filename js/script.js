d00aad53f2caaf9f5c4ad442693674d242666198
ian <ian.miell@container-solutions.com>
2023-06-26 11:22:28 +0100

    remove magic number

--- js/script.js
+++ js/script.js
@@ -80,7 +80,7 @@ for (var i = 0; i < row.length; i++) {
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = 'rgba(169,243,230,.2)';
-    ctx.fillRect(marginLeft + cellWidth, cellHeight, cellWidth * 3.5, cellHeight * 9);
+    ctx.fillRect(marginLeft + cellWidth, cellHeight, cellWidth * 3.5, cellHeight * row.length);
     ctx.setLineDash([6, 4]);
     ctx.beginPath();
     ctx.moveTo(marginLeft + cellWidth, cellHeight);
